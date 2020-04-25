9d45786c93001d413259be758105049d31eb1c02
IuliaRobas <iulia.robas@yahoo.com>
2020-04-25 22:52:53 +0300

    added env config

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://dbUser:dbPassword@mern-tutorial-sz7yw.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("connected to DB"))
   .catch(error => console.log(error));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json("heya");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((error, userData) => {
+    if (error) return res.json({ success: false, error });
+
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(5000);
